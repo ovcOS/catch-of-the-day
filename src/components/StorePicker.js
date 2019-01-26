4ff797702ab5d430281ab966e59cb904764c4951
Oriol VidalCortes <ovidalcortes@gmail.com>
2019-01-26 21:16:12 +0100

    Basic layout

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
-    render() {return <p>My first react component!</p>}
+    render() {
+        return (
+           <form className="store-selector">
+           <h2>Enter a store</h2>
+           <input type="text" placeholder="Store name"/>
+           <button type="submit">Create! </button>
+           </form>
+        )
+    }
 }
 
 export default StorePicker;
